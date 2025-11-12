ea5a2d6b041385b5d01b8a5e8372217ebba5914b
ryanm <ryanmaxwellnyy@gmail.com>
2025-11-12 16:44:08 -0500

    modified calculator p1

--- calculator.py
+++ calculator.py
@@ -9,22 +9,3 @@ import math
 
 def add(a, b):
     return a + b
-
-def sub(a, b):
-    return a - b
-
-def mul(a, b):
-    return a * b
-
-def div(a, b):
-    if b == 0:
-        raise ZeroDivisionError("Cannot divide by zero.")
-    return a / b
-
-def log(a, b):
-    if a <= 0 or b <= 0 or b == 1:
-        raise ValueError("Logarithm domain error: a and b must be positive, and b cannot be 1.")
-    return math.log(a, b)
-
-def exp(a, b):
-    return a ** b
