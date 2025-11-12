17a6490ca9d2dc81ab02f1f53ee5f9f656afe71a
ryanm <ryanmaxwellnyy@gmail.com>
2025-11-12 17:13:05 -0500

    add names

--- test_calculator.py
+++ test_calculator.py
@@ -2,35 +2,39 @@
 # Partner 1: Ryan Leitner
 # Partner 2: Aung Sett
 
-from calculator import add, subtract, mul, div, logarithm, exp, hypotenuse, square_root
-import pytest
-import calculator
 import math
+import unittest
+import calculator
+from calculator import add, subtract, mul, div, logarithm, exp, hypotenuse, square_root
 
-def test_add():
-    assert calculator.add(2, 3) == 5
-    assert calculator.add(-1, 1) == 0
 
-def test_subtract():
-    assert calculator.subtract(5, 3) == 2
-    assert calculator.subtract(0, 4) == -4
+class TestCalculator(unittest.TestCase):
 
-def test_divide_by_zero():
-    with pytest.raises(ZeroDivisionError):
-        calculator.div(5, 0)
+    def test_add(self):
+        self.assertEqual(add(2, 3), 5)
+        self.assertEqual(add(-1, 1), 0)
 
-def test_logarithm():
-    assert math.isclose(calculator.logarithm(10, 100), 2)
-    assert math.isclose(calculator.logarithm(2, 8), 3)
+    def test_subtract(self):
+        self.assertEqual(subtract(5, 3), 2)
+        self.assertEqual(subtract(0, 4), -4)
 
-def test_log_invalid_base():
-    with pytest.raises(ValueError):
-        calculator.logarithm(1, 10)
-    with pytest.raises(ValueError):
-        calculator.logarithm(-2, 8)
-    with pytest.raises(ValueError):
-        calculator.logarithm(2, -8)
- def test_hypotenuse(self):  # 3 assertions
+    def test_divide_by_zero(self):
+        with self.assertRaises(ZeroDivisionError):
+            div(5, 0)
+
+    def test_logarithm(self):
+        self.assertAlmostEqual(logarithm(10, 100), 2)
+        self.assertAlmostEqual(logarithm(2, 8), 3)
+
+    def test_log_invalid_base(self):
+        with self.assertRaises(ValueError):
+            logarithm(1, 10)
+        with self.assertRaises(ValueError):
+            logarithm(-2, 8)
+        with self.assertRaises(ValueError):
+            logarithm(2, -8)
+
+    def test_hypotenuse(self):  # 3 assertions
         self.assertAlmostEqual(hypotenuse(3, 4), 5.0)
         self.assertAlmostEqual(hypotenuse(-3, 4), 5.0)
         self.assertAlmostEqual(hypotenuse(0, 0), 0.0)
@@ -40,5 +44,7 @@ def test_log_invalid_base():
         self.assertAlmostEqual(square_root(9), 3.0)
         with self.assertRaises(ValueError):
             square_root(-1)
+
+
 if __name__ == "__main__":
     unittest.main()
