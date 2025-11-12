1b8b891be750f810c58a0d9380590e27da13bad4
ryanm <ryanmaxwellnyy@gmail.com>
2025-11-12 17:01:34 -0500

    add names

--- test_calculator.py
+++ test_calculator.py
@@ -15,11 +15,17 @@ class TestCalculator(unittest.TestCase):
     # ##########################
 
     ######## Partner 1
-    # def test_multiply(self): # 3 assertions
-    #     fill in code
+    def test_multiply(self):  # 3 assertions
+        self.assertEqual(mul(3, 4), 12)
+        self.assertEqual(mul(5, 0), 0)
+        self.assertEqual(mul(-2, 6), -12)
 
-    # def test_divide(self): # 3 assertions
-    #     fill in code
+    def test_divide(self):  # 3 assertions
+        self.assertAlmostEqual(div(10, 2), 5.0)
+        self.assertAlmostEqual(div(3, 2), 1.5)
+        with self.assertRaises(ZeroDivisionError):
+            div(5, 0)
+
     # ##########################
 
     ######## Partner 2
@@ -38,21 +44,19 @@ class TestCalculator(unittest.TestCase):
     # ##########################
     
     ######## Partner 1
-    # def test_log_invalid_argument(self): # 1 assertion
-    #     # call log function inside, example:
-    #     # with self.assertRaises(<INSERT_ERROR_TYPE>):
-    #     #     logarithm(0, 5)
-    #     fill in code
+    def test_log_invalid_argument(self):  # 1 assertion
+        with self.assertRaises(ValueError):
+            log(0, 5)
+    def test_hypotenuse(self):  # 3 assertions
+        self.assertAlmostEqual(hypotenuse(3, 4), 5.0)
+        self.assertAlmostEqual(hypotenuse(-3, 4), 5.0)
+        self.assertAlmostEqual(hypotenuse(0, 0), 0.0)
 
-    # def test_hypotenuse(self): # 3 assertions
-    #     fill in code
-
-    # def test_sqrt(self): # 3 assertions
-    #     # Test for invalid argument, example:
-    #     # with self.assertRaises(<INSERT_ERROR_TYPE>):
-    #     #    square_root(NUM)
-    #     # Test basic function
-    #     fill in code
+    def test_sqrt(self):  # 3 assertions
+        self.assertAlmostEqual(square_root(4), 2.0)
+        self.assertAlmostEqual(square_root(9), 3.0)
+        with self.assertRaises(ValueError):
+            square_root(-1)
     ##########################
 
 # Do not touch this
