eba4c1716cf2db97791e039c53fafbff2996e99a
kaung <aungsett17co@gmail.com>
2025-11-12 16:44:45 -0500

    Partner 2 Code

--- calculator.py
+++ calculator.py
@@ -1,11 +1,27 @@
-"""
-calculator.py
-- Defines functions used to create a simple calculator
+# https://github.com/<your-repo-link>
+# Partner 1: Aung Sett
+# Partner 2: Ryan Leitner
 
-One function per operation, in order.
-"""
-# First example
-def add(a, b): 
-    pass
+import math
 
+def add(a, b):
+    return a + b
 
+def sub(a, b):
+    return a - b
+
+def mul(a, b):
+    return a * b
+
+def div(a, b):
+    if b == 0:
+        raise ZeroDivisionError("Cannot divide by zero")
+    return a / b
+
+def log(a, b):
+    if a <= 0 or a == 1 or b <= 0:
+        raise ValueError("Invalid input for logarithm")
+    return math.log(b, a)
+
+def exp(a, b):
+    return a ** b
